d5a745e258a137403ae7208193ee2e1a39373d8d
Elsan <Elsan.memeti@hotmail.com>
2020-01-26 21:26:55 +0100

    Register-maprout

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,7 +88,7 @@ namespace JobFinder.Areas.Identity.Pages.Account
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl);
+                    return RedirectToRoute("Posts");
                 }
                 foreach (var error in result.Errors)
                 {
